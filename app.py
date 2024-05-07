6467baa5ab5cf9f6a3abbbd64922222c46e2823b
“a-hersa” <alberto.hernandez@protonmail.com>
2024-05-07 21:26:08 +0200

    Added calculadora.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 
 app=Flask(__name__)
 
@@ -6,8 +6,17 @@ app=Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/calculadora', methods=['GET','POST'])
+def calculadora():
+    if request.method=='GET': 
+        return render_template('form.html')
 
+    else:
+        url=request.form['url']
+        itp=int(int(50000)*0.1)
+        return render_template('calculadora.html', itp=itp)
 
 if __name__=='__main__':
     # app.run(host='0.0.0.0',debug=True)
-    app.run(host='0.0.0.0',port=8080)
+    # app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
---
+++ src/scraper.py
@@ -0,0 +1,3 @@
+import os
+import requests
+from bs4 import beautifulsoup4
