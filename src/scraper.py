f5bc9a85a82cb3847337377d028f6d8c75a30713
“a-hersa” <alberto.hernandez@protonmail.com>
2024-05-09 20:21:19 +0200

    Improved calculadora template

--- src/scraper.py
+++ src/scraper.py
@@ -7,9 +7,8 @@ def scraper(url):
     req_url = f'https://api.scrapingant.com/v2/general?url={encoded_url}&x-api-key={os.getenv("SCRAPINGANT")}&browser=false&proxy_country=ES'
     r = requests.get(req_url)
     soup = BeautifulSoup(r.text)
-    datos = []
     nombre = soup.find('span', class_='main-info__title-main').text
     precio = soup.find('span', class_='info-data-price').text.replace('.','').strip(' €')
     metros = soup.find('div', class_='info-features').span.text.strip('\n').replace(' m²','')
-    datos.extend([nombre, precio, metros])
+    datos = [nombre, precio, metros]
     return datos
