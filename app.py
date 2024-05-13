ec6c0930798d2a5b42b382486f2b82f1e3b6c262
“a-hersa” <alberto.hernandez@protonmail.com>
2024-05-13 23:58:32 +0200

    First working version of DatosCalculadora class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from src.scraper import scraper
+from src.calculadora import DatosCalculadora
 
 app=Flask(__name__)
 
@@ -7,15 +8,12 @@ app=Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-@app.route('/calculadora', methods=['GET','POST'])
+@app.route('/calculadora', methods=['POST'])
 def calculadora():
-    if request.method=='GET': 
-        return render_template('calculadora.html')
-
-    else:
-        url=request.form['url']
-        datos = scraper(url)
-        return render_template('calculadora.html', url=url, datos=datos)
+    url=request.form['url']
+    scrape = scraper(url)
+    datos = DatosCalculadora(scrape[0], scrape[1], scrape[2])
+    return render_template('calculadora.html', url=url, datos=datos)
 
 if __name__=='__main__':
     # app.run(host='0.0.0.0',debug=True)
