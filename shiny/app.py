9504f59754c0d7f13c92d673990badcf31fec060
a-hersa <alberto.hernandez@protonmail.com>
2025-02-04 17:15:13 +0100

    first working chart with postgres connection

--- shiny/app.py
+++ shiny/app.py
@@ -1,46 +1,64 @@
+from pathlib import Path
 import pandas as pd
+
+# Primer ejemplo de histograma simple
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from shiny.express import ui, input, render
+
+# Segundo ejemplo de dos gráficos con plotly
+import plotly.express as px
+from shiny import reactive
+from shiny.express import render, input, ui
+from shinywidgets import render_plotly
+
 from sqlalchemy import create_engine
-from shiny import App, render, ui
-from plotnine import ggplot, aes, geom_bar, labs
+# from shiny import App, render, ui
+# from plotnine import ggplot, aes, geom_bar, labs
 import os
 from dotenv import load_dotenv
 
-# # Cargar las variables de entorno desde el archivo .env
-# load_dotenv()
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
-# # Configuración de la conexión a PostgreSQL con SQLAlchemy
-# DB_URI = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@inmobiliario-postgres:5432/inmobiliario_db'
+# Configuración de la conexión a PostgreSQL con SQLAlchemy
+DB_URI = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@inmobiliario-postgres:5432/inmobiliario_db'
 
-# # Crear la conexión con SQLAlchemy
-# engine = create_engine(DB_URI)
+# Crear la conexión con SQLAlchemy
+engine = create_engine(DB_URI)
 
-from pathlib import Path
+ui.page_opts(title="Análisis inmobiliario del rango 0 - 120k", fillable=True)
 
-import matplotlib.pyplot as plt
-import numpy as np
-from shiny.express import ui, input, render
+# @reactive.calc
+# def dat():
+#     infile = Path(__file__).parent / "propiedades.csv"
+#     return pd.read_csv(infile)
 
-file_path = Path(__file__).parents[0] / "propiedades.csv"
-df = pd.read_csv(file_path)
+# Función para obtener datos de la base de dato
+@reactive.calc
+def fetch_data():
+    try:
+        query = "SELECT * FROM propiedades;"
+        data = pd.read_sql_query(query, engine)
+        return data
+    except Exception as e:
+        print(f"Error al conectar con la base de datos: {e}")
+        return pd.DataFrame()
 
-with ui.sidebar():
-    ui.input_slider("n", "N", 0, 100, 20)
+with ui.layout_columns():
 
+    @render_plotly
+    def plot1():
+        df = fetch_data()
+        return px.bar(df, x="poblacion", y="precio")
 
-@render.plot(alt="A histogram")
-def histogram():
-    propiedades = df["precio"].values
-    plt.hist(propiedades, input.n(), density=True)
+    # @render_plotly
+    # def plot2():
+    #     return px.histogram(px.data.habitaciones(), y="habitaciones")
 
-# # Función para obtener datos de la base de datos
-# def fetch_data():
-#     try:
-#         query = "SELECT poblacion, precio FROM propiedades;"
-#         data = pd.read_sql_query(query, engine)
-#         return data
-#     except Exception as e:
-#         print(f"Error al conectar con la base de datos: {e}")
-#         return pd.DataFrame()
+    # @render.data_frame
+    # def data():
+    #     return dat()
 
 # # UI de la aplicación
 # app_ui = ui.page_fluid(
